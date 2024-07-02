ac86852756f377c0f707912b22810e208c78834b
Deepak <deepakmuthu1122@gmail.com>
2024-07-02 21:47:26 +0530

    Products in Home Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import { Container } from "react-bootstrap";
+import { Outlet } from 'react-router-dom';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+// import { useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { logout } from './slices/authSlice';
+
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <Container>
+        <Outlet/>
+      </Container>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
