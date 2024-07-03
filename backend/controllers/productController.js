4774695ee83a0fb665f49708a95be6c5e1063e8d
Deepak <deepakmuthu1122@gmail.com>
2024-07-03 18:48:49 +0530

    Redux, Redux toolkit and api slice setup

---
+++ backend/controllers/productController.js
@@ -0,0 +1,24 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  public
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.send(products);
+});
+
+// @desc    Fetch a single products
+// @route   GET /api/products/:id
+// @access  public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) return res.send(product);
+  else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export { getProducts, getProductById };
