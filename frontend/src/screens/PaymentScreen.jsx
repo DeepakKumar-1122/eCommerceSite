8be34b69302d254c03836613b083625b7e613fb0
Deepak <deepakmuthu1122@gmail.com>
2024-07-06 22:09:33 +0530

    Payment screen

--- frontend/src/screens/PaymentScreen.jsx
+++ frontend/src/screens/PaymentScreen.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Form, Button, Col } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import FormContainer from '../components/FormContainer';
+import CheckoutSteps from '../components/CheckoutSteps';
+import { setPaymentMethod } from '../slices/shippingSlice';
 
 const PaymentScreen = () => {
-  return (
-    <div>PaymentScreen</div>
-  )
-}
+  const navigate = useNavigate();
+  const shipping = useSelector((state) => state.shipping);
+  const { shippingAddress } = shipping;
 
-export default PaymentScreen
+  useEffect(() => {
+    if (!shippingAddress.address) {
+      navigate('/shipping');
+    }
+  }, [navigate, shippingAddress]);
+
+  const [payment, setPayment] = useState('PayPal');
+
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(setPaymentMethod(payment));
+    navigate('/placeorder');
+  };
+
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 />
+      <h1>Payment Method</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group>
+          <Form.Label as='legend'>Select Method</Form.Label>
+          <Col>
+            <Form.Check
+              className='my-2'
+              type='radio'
+              label='PayPal or Credit Card'
+              id='PayPal'
+              name='paymentMethod'
+              value='PayPal'
+              checked
+              onChange={(e) => setPayment(e.target.value)}
+            ></Form.Check>
+          </Col>
+        </Form.Group>
+
+        <Button type='submit' variant='primary'>
+          Continue
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default PaymentScreen;
--- frontend/src/slices/shippingSlice.js
+++ frontend/src/slices/shippingSlice.js
@@ -1,34 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { updateCart } from '../utils/cartUtils';
+import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils";
 
-const initialState = {
-  shippingAddress: {
-    address: '',
-    city: '',
-    postalCode: '',
-    country: ''
-  },
-  orderItems: [],
-  itemsPrice: 0,
-  shippingPrice: 0,
-  taxPrice: 0,
-  totalPrice: 0
-};
+const initialState = localStorage.getItem("shipping")
+  ? JSON.parse(localStorage.getItem("shipping"))
+  : {
+      orderItems: [],
+      shippingAddress: {},
+      paymentMethod: "",
+      itemsPrice: 0,
+      shippingPrice: 0,
+      taxPrice: 0,
+      totalPrice: 0,
+    };
 
 const shippingSlice = createSlice({
-  name: 'shipping',
+  name: "shipping",
   initialState,
   reducers: {
     setShippingAddress: (state, action) => {
       state.shippingAddress = action.payload;
-      localStorage.setItem('shippingAddress', JSON.stringify(state.shippingAddress));
+      localStorage.setItem("shipping", JSON.stringify(state));
     },
     setOrderItems: (state, action) => {
       state.orderItems = action.payload;
       updateCart(state);
+      localStorage.setItem("shipping", JSON.stringify(state));
     },
     setPrices: (state) => {
       updateCart(state);
+      localStorage.setItem("shipping", JSON.stringify(state));
+    },
+    setPaymentMethod: (state, action) => {
+      state.paymentMethod = action.payload;
+      localStorage.setItem("shipping", JSON.stringify(state));
     },
     resetShipping: (state) => {
       state.shippingAddress = initialState.shippingAddress;
@@ -37,12 +41,18 @@ const shippingSlice = createSlice({
       state.shippingPrice = initialState.shippingPrice;
       state.taxPrice = initialState.taxPrice;
       state.totalPrice = initialState.totalPrice;
-      localStorage.removeItem('shippingAddress');
-      localStorage.removeItem('cart');
-    }
-  }
+      localStorage.removeItem("shipping");
+      // localStorage.removeItem('cart');
+    },
+  },
 });
 
-export const { setShippingAddress, setOrderItems, setPrices, resetShipping } = shippingSlice.actions;
+export const {
+  setShippingAddress,
+  setOrderItems,
+  setPrices,
+  setPaymentMethod,
+  resetShipping,
+} = shippingSlice.actions;
 
 export default shippingSlice.reducer;
